ef5e89c298c9728ba1568e73881eea900149b56e
Gabriel Esquilage <roccaesquil19910@gmail.com>
2025-07-10 17:49:08 -0300

    criação do automaticovalidateste_Lp de Pós

--- cypress/e2e/validaCadastroLpPos/cadLppos.cy.js
+++ cypress/e2e/validaCadastroLpPos/cadLppos.cy.js
@@ -24,5 +24,94 @@ describe('Valida Cadastro de Processo Seletivo', () => {
             .type('rebecajlutz@armyspy.com');
 
         cy.contains('button', 'Próximo').click()
+
+//step 2
+
+        cy.contains('label', 'Digite seu CEP *')
+            .parent() // sobe para o container do campo- teste (deu certo, replicar para proximos)
+            .within(() => {
+        cy.get('input[id="cep"]').type('87080400');
+
+    // Aguarda até o botão "Aplicar endereço" estar visível e habilitado
+        //cy.contains('button', 'Aplicar endereço')
+          //  .should('be.visible')       // o botão precisa estar visível
+            //.and('not.be.disabled')     // e habilitado
+            //.click();
+      });
+
+         //cy.get('input[placeholder="Digite o endereço do Lead"]')
+      //      .should('be.visible')
+        //    .type('rrua rua rua');
+
+        cy.get('input[placeholder="Digite o número"]')
+            .should('be.visible')
+            .type('123');
+
+            cy.wait(1500)
+        cy.get('.ml-auto').click()
+
+//step3
+
+        cy.get('#«rd»-form-item').click()
+
+        cy.get('[data-value="ESPECIALIZAÇÃO EM AGRICULTURA E MANEJO SUSTENTÁVEL"]').click();
+
+        cy.contains('label', 'Estado *')
+            .parent() // sobe para o container do campo- teste (deu certo, replicar para proximos)
+            .within(() => {
+        cy.contains('div', 'Selecione um Estado').click();
+        })
+
+        cy.contains('div', 'Acre').click()
+
+//--------------------------------------//        
+
+        cy.contains('label', 'Cidade *')
+            .parent() // sobe para o container do campo- teste (deu certo, replicar para proximos)
+            .within(() => {
+        cy.contains('div', 'Selecione uma cidade').click();
+        })
+
+        cy.contains('div', 'Acrelândia').click()
+
+//--------------------------------------//
+
+        cy.contains('label', 'Qual polo prefere estudar? *')
+            .parent() // sobe para o container do campo- teste (deu certo, replicar para proximos)
+            .within(() => {
+        cy.contains('div', 'Selecione um Polo').click();
+        })
+
+        cy.contains('div', 'ACRELÂNDIA - AC').click()
+
+//--------------------------------------//
+
+        cy.contains('label', 'Planos de Pagamento *')
+            .parent() // sobe para o container do campo- teste (deu certo, replicar para proximos)
+            .within(() => {
+        cy.contains('div', 'Selecione plano de pagamento').click();
+        })
+
+        cy.contains('div', '1 x 29.9 + 1 x 328.9').click()
+
+//--------------------------------------//
+
+        cy.contains('label', 'Dia de vencimento das mensalidades *')
+            .parent() // sobe para o container do campo- teste (deu certo, replicar para proximos)
+            .within(() => {
+        cy.contains('div', 'Selecione dia de vencimento').click();
+        })
+
+        cy.contains('div', '10').click()
+
+//-------------------------------------//
+
+        cy.get('input[placeholder="Digite o código do cupom"]')
+            .should('be.visible')
+            .type('teste100');
+
+            cy.wait(1500)
+        //cy.get('.ml-auto').click()
+
     })    
 });        
